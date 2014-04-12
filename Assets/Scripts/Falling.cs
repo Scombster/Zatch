ca370fec84deb89c26e0f02a8efc4e3a74d7b738
Bearukun <bearukun@gmail.com>
2014-04-12 02:01:05 +0200

    Big update, see description!
    ==Modifications made to Unity project==
    
    Added rest of the bricks // Prefabbin' and stuff.
    Corrected size of brick images, also corrected proportions - now
    100x100.
    
    ==Modification made to Falling.cs==
    Added temp. left and right movement. (To left and right key)
    Added temp. rotate. (To up key)
    Added temp. soft drop. (To down key)
    Now the controlled brick only can collide with something under it.
    -BUG- You can push to bricks, this needs fixing!

--- Assets/Scripts/Falling.cs
+++ Assets/Scripts/Falling.cs
@@ -2,10 +2,18 @@
 using System.Collections;
 
 public class Falling : MonoBehaviour {
+	//This is part of harddrop
+	//Target pointing to ButtomLayer
+	//public Transform target;
+	//Hard drop speed
+	//float hardSpeed = 1.5f;
+
+
 
 	float MoveCD = 0f;
-
 	float Speed = 0.5f;
+
+
 
 	// Use this for initialization
 	void Start () {
@@ -15,20 +23,37 @@ public class Falling : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		/*
-		float horz = Input.GetAxis ("Horizontal");
-		Vector3 velocity = rigidbody2D.velocity;
-		velocity.x = horz;
-		rigidbody2D.velocity = velocity;
-		*/
-
-		//Should be deltatime because of FPS
-		if (MoveCD < Time.time)
-		{
-			transform.position += new Vector3 (0, -1f, 0);
-			Move ();
+		//Temp. controller scipt //Bear out
+		if(Input.GetKeyDown("left")){
+			transform.position += new Vector3(-1f,0,0);
+		}
+		if(Input.GetKeyDown("right")){
+			transform.position += new Vector3(1f,0,0);
+		}
+		if(Input.GetKeyDown("up")){
+			//rotate
+			transform.Rotate (Vector3.forward * 90);
+		}
+		if(Input.GetKeyDown("down")){
+			transform.position += new Vector3(0,-1f,0);
+		}
+		if(Input.GetKeyDown("space")){
+			//hard drop, just an idea to use MoveTowards
+			//transform.position = Vector3.MoveTowards(transform.position, target.position, hardSpeed*Time.deltaTime);
 		}
 
+
+
+
+
+
+		//Should be deltatime because of FPS?
+		if (MoveCD < Time.time)
+		{
+			transform.position += new Vector3(0, -1f, 0);
+			Move ();
+		}
+		//
 		/*if (Time.time >= 5f) 
 		{
 			Speed = 0.2f;	
@@ -39,8 +64,21 @@ public class Falling : MonoBehaviour {
 	}
 
 	void OnCollisionEnter2D(Collision2D collision){
-		this.enabled = false;
-		transform.position += new Vector3 (0, 0, 0);
+		//If-statement that only makes it possible for the brick to collide with objects under it. 
+		if(collision.contacts.Length > 0)
+		{
+			ContactPoint2D contact = collision.contacts[0];
+			if(Vector3.Dot(contact.normal, Vector3.up) > 0.5)
+			{
+				this.enabled = false;
+				transform.position += new Vector3(0, 0, 0);
+			}
+		}
+
+
+
+
+
 	}
 
 	void Move()
