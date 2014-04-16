94bb2a28fcae16de4b8a906f4c46708f05b7f99e
Scombster <kofod88@gmail.com>
2014-04-17 00:14:21 +0200

    New script "ScoreController" & "Collide" update
    *Added gameobject "Score" and script "ScoreController"
    *Updated "Collide" script:
    - Score from ScoreController is initialized in "Collide"
    - Bricks colliding with bricks of the same color will get destroyed and
    a score of 1 is added for each brick.

--- Assets/Scripts/Collide.cs
+++ Assets/Scripts/Collide.cs
@@ -3,38 +3,60 @@ using System.Collections;
 
 public class Collide : MonoBehaviour {
 
-	int score;
-
+	private ScoreController score;
 
 	// Use this for initialization
-	void Start () {
-	
-
-
-	
-	
+	void Start () 
+	{
+		score = GameObject.Find("Score").GetComponent<ScoreController>();	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
-				if (Input.GetKey (KeyCode.Delete)) {
-						Destroy (gameObject);
-				}
+	void Update () 
+	{			
 
-		//Start of tracking and displaying Score
-		//guiText.text = "This is the Score";
+		if (Input.GetKey (KeyCode.Delete)) 
+		{
+			Destroy (gameObject);
+		}
+		
 	}
 
-	//Collision between objects with same tag
-	void OnCollisionEnter(Collision other){
+	//Collision and score update between objects with same tag
+	void OnCollisionEnter2D(Collision2D other)
+	{
 			
-			if(other.gameObject.tag == "Blue"){
-				
-				Destroy(gameObject);
-				score++;
-			}
+		if(this.gameObject.tag == "Blue" && other.gameObject.tag == "Blue")
+		{
+			Destroy(gameObject);
+			score.score += 1;
 		}
+
+		if(this.gameObject.tag == "Green" && other.gameObject.tag == "Green")
+		{
+			Destroy(gameObject);
+			score.score += 1;
+		}
+
+		if(this.gameObject.tag == "Orange" && other.gameObject.tag == "Orange")
+		{
+			Destroy(gameObject);
+			score.score += 1;
+		}
+
+		if(this.gameObject.tag == "Yellow" && other.gameObject.tag == "Yellow")
+		{
+			Destroy(gameObject);
+			score.score += 1;
+		}
+
+		if(this.gameObject.tag == "Purple" && other.gameObject.tag == "Purple")
+		{
+			Destroy(gameObject);
+			score.score += 1;
+		}
+
+	}
 
 		
 }
---
+++ Assets/Scripts/ScoreController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreController : MonoBehaviour {
+
+	public int score = 0;
+
+	// Use this for initialization
+	void Awake () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		guiText.text = "Score: " + score;
+
+	}
+}
