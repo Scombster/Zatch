b6a52c3f78fe5ae52a47560486f18ac58e664ba1
BrugernavnTaken <mpajon12@student.aau.dk>
2014-04-22 14:52:06 +0200

    Functional Menu :D

---
+++ Assets/Scripts/Play.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Play : MonoBehaviour {
+
+	public string Level;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+
+	}
+
+	void OnMouseUp () {
+
+
+			Application.LoadLevel (Level);
+		Debug.Log ("click");
+
+				
+		}
+
+
+}
