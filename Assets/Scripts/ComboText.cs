4afb9344ad5d946284e6fa3126f7651210be003b
Scombster <kofod88@gmail.com>
2014-04-25 13:08:17 +0200

    ComboText Update
    *Changed script to match new combo system and replaced if statements
    with a switch

--- Assets/Scripts/ComboText.cs
+++ Assets/Scripts/ComboText.cs
@@ -3,29 +3,50 @@ using System.Collections;
 
 public class ComboText : MonoBehaviour {
 	
-	//public int comboColor = 0;
+	private ScoreController comboColor;
 
-	/*
-	private ScoreController comboBlue;
-	private ScoreController comboGreen;
-	private ScoreController comboOrange;
-	private ScoreController comboYellow;
-	private ScoreController comboPurple;
-	*/
 	// Use this for initialization
 	void Start () {
-		
+		comboColor = GameObject.Find("Score").GetComponent<ScoreController>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	/*	
-		comboBlue = GameObject.Find("Score").GetComponent<ScoreController>();
-		comboGreen = GameObject.Find("Score").GetComponent<ScoreController>();
-		comboOrange = GameObject.Find("Score").GetComponent<ScoreController>();
-		comboYellow = GameObject.Find("Score").GetComponent<ScoreController>();
-		comboPurple = GameObject.Find("Score").GetComponent<ScoreController>();
-	*/
+
+		comboColor = GameObject.Find("Score").GetComponent<ScoreController>();
+
+		switch (comboColor.comboColor) 
+		{
+				case 0:
+						guiText.text = "No Combo";
+						guiText.color = Color.blue;
+						break;
+
+				case 1:
+						guiText.text = "BLUE COMBO!";
+						guiText.color = Color.blue;
+						break;
+
+				case 2:
+						guiText.text = "GREEN COMBO!";
+						guiText.color = Color.green;
+						break;
+
+				case 3:
+						guiText.text = "ORANGE COMBO!";
+						guiText.color = Color.magenta;
+						break;
+
+				case 4:
+						guiText.text = "YELLOW COMBO!";
+						guiText.color = Color.yellow;
+						break;
+
+				case 5:
+						guiText.text = "PURPLE COMBO!";
+						guiText.color = Color.red;
+						break;
+		}
 
 		/*
 		if(comboBlue.comboBlue > 0)
