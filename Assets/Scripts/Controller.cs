c4c911043d5ece83007567d7b70ebac19ac3746d
BrugernavnTaken <mpajon12@student.aau.dk>
2014-04-27 17:46:18 +0200

    Trying to fix Harddrop with Bjørn

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -2,19 +2,20 @@
 using System.Collections;
 
 public class Controller : MonoBehaviour {
-
+	
 	//Controlling how fast the keyDown will be.
 	private float lastDown, timeBetweenDowns = 0.5f;
-
-
+	public float speed;
+	public Transform target;
+	
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
+		
 		//Temp. controller scipt //Bear out
 		if(Input.GetKeyDown("left")){
 			transform.position += new Vector3(-1f,0,0);
@@ -27,32 +28,25 @@ public class Controller : MonoBehaviour {
 			transform.Rotate (Vector3.forward * 90);
 		}
 		if(Input.GetKeyDown("down")){
-
+			
 			transform.position += new Vector3(0,-1f,0);
-
-
+			
+			
 		}
 		if(Input.GetKeyDown("space")){
-			//hard drop, just an idea to use MoveTowards
-			//transform.position = Vector3.MoveTowards(transform.position, target.position, hardSpeed*Time.deltaTime);
-
-			//Simple Harddrop mechanic
-			transform.position += new Vector3(0,-15f,0);
+			
+			
+			float step = speed * Time.deltaTime;
+			transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+			
+			//transform.position += new Vector3(0,-15f,0);
 		}
-
+		
 		//"Resets" the game by destroying all bricks
 		if (Input.GetKey (KeyCode.Delete)) {
 			Destroy (gameObject);
 		}
-
-
+		
 	}
-
-
-
-
-
-
-
-
+	
 }
