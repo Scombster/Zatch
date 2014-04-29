fe7f516e9f05faa12ba943baa9cefc33726b1f62
Scombster <kofod88@gmail.com>
2014-04-29 15:19:41 +0200

    GUItext Test
    SCRIPT
    *Updated LevelDisplay - Trying new formula for text position

--- Assets/Scripts/LevelDisplay.cs
+++ Assets/Scripts/LevelDisplay.cs
@@ -4,12 +4,14 @@ using System.Collections;
 public class LevelDisplay : MonoBehaviour {
 
 	private TimeController level;
+	private float width = 0 + (Screen.width/5);
 
 	// Use this for initialization
 	void Start () {
 		level = GameObject.Find("Timer").GetComponent<TimeController>();
 		guiText.fontSize = 20;
-		guiText.pixelOffset = new Vector2 (Screen.width/2 -300, Screen.height/2 -275);
+		//guiText.pixelOffset = new Vector2 (Screen.width/2 -300, Screen.height/2 -275);
+		guiText.pixelOffset = new Vector2 (width, Screen.height/2 -275);
 	}
 
 	// Update is called once per frame
